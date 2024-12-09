832d1d2c8218a918b3f2b2bbded0bf499440dd4e
Dhameliya Priyam <121245602+priyam9699@users.noreply.github.com>
2024-12-09 14:27:02 +0530

    Update urls.py

--- ecomProject/urls.py
+++ ecomProject/urls.py
@@ -27,6 +27,4 @@ urlpatterns = [
 # Serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if not settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
