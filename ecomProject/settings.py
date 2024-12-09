b403161f177dad4901a000152d93ec21ff9e704d
Dhameliya Priyam <121245602+priyam9699@users.noreply.github.com>
2024-12-09 12:36:39 +0530

    Update settings.py

--- ecomProject/settings.py
+++ ecomProject/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3e125$k#x#b6lu5ge!^@iy41=0y7z_!o==!-kto)03w!2e_b&e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['oyster-app','ecomdetective.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['seal-app-vojox.ondigitalocean.app','ecomdetective.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
